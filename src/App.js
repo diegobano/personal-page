6b3baa8b944f238cefc6adac7cde79d3654f2ef1
Diego Baño <diego.bano@ug.uchile.cl>
2020-07-15 03:22:11 -0400

    New message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import AddTodo from "./components/AddTodo";
+// import TodoList from "./components/TodoList";
+// import VisibilityFilters from "./components/VisibilityFilters";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="todo-app">
+      <h1>Bienvenido a mi página!</h1>
+      <h2>Aún está en construcción, un poco de paciencia por favor!</h2>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-import TodoApp from "./TodoApp";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp />
+    <App />
   </Provider>,
   rootElement
 );
